e4c3ba2b4c2276e9df7cb8da43c3fefc3a6219d3
Frejus DASSI <frejohope@gmail.com>
2025-01-16 16:07:38 +0100

    la relation @OneToMany

--- src/main/java/bj/highfiveuniversity/book/models/Categorie.java
+++ src/main/java/bj/highfiveuniversity/book/models/Categorie.java
@@ -1,10 +1,13 @@
 package bj.highfiveuniversity.book.models;
 
+import java.util.Set;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 
@@ -17,8 +20,11 @@ public class Categorie {
     private Long id;
 
     @Column(nullable = false)
-    private String nom;   
-
+    private String nom;
+    
+    @OneToMany(mappedBy = "categories")
+    private Set<Book> Livres;
+    
     public Long getId() {
         return id;
     }
