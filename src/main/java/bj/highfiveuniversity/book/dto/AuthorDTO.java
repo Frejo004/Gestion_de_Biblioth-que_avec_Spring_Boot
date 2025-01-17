f2e9f670055b57f61655e1317d6b170d66c47703
Frejus DASSI <frejohope@gmail.com>
2025-01-17 15:32:22 +0100

    création de DTO pour author

---
+++ src/main/java/bj/highfiveuniversity/book/dto/AuthorDTO.java
@@ -0,0 +1,54 @@
+package bj.highfiveuniversity.book.dto;
+
+public class AuthorDTO {
+    private Long id;
+    private String nom;
+    private String prenom;
+    private String nationalite;
+
+    //construtor
+    public AuthorDTO(Long id, String nom, String prenom, String nationalite) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.nationalite = nationalite;
+    }
+
+
+    //Getters & Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getNationalite() {
+        return nationalite;
+    }
+
+    public void setNationalite(String nationalite) {
+        this.nationalite = nationalite;
+    }
+
+    
+
+    
+}
