19fccb1faf0afe96170ffbeb5829dc765543b681
Frejus DASSI <frejohope@gmail.com>
2025-01-17 18:12:08 +0100

    création du fichier BookMapper

---
+++ src/main/java/bj/highfiveuniversity/book/mapper/BookMapper.java
@@ -0,0 +1,12 @@
+package bj.highfiveuniversity.book.mapper;
+
+import bj.highfiveuniversity.book.dto.BookDTO;
+import bj.highfiveuniversity.book.models.Book;
+
+public class BookMapper {
+    public static BookDTO toDto(Book livre) {
+
+        return new BookDTO(livre.getId(), livre.getTitle(), livre.getIsbn(), livre.getAuteurs());
+    }
+
+}
