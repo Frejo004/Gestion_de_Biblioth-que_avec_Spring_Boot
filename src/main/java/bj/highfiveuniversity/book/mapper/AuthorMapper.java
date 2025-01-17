bf4d46932120b5597d0ea393aaa1f624751023f9
Frejus DASSI <frejohope@gmail.com>
2025-01-17 18:11:33 +0100

    création du fichier AuthorMapper

---
+++ src/main/java/bj/highfiveuniversity/book/mapper/AuthorMapper.java
@@ -0,0 +1,12 @@
+package bj.highfiveuniversity.book.mapper;
+
+import bj.highfiveuniversity.book.dto.AuthorDTO;
+import bj.highfiveuniversity.book.models.Author;
+
+public class AuthorMapper {
+
+    public static AuthorDTO toDto(Author auteur) {
+        
+        return new AuthorDTO(auteur.getId(), auteur.getNom(), auteur.getPrenom());
+    }
+}
